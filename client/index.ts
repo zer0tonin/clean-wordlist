45006a8337110b82833aaefb7d86ab71990c5777
Alice GG <hi@alicegg.tech>
2023-11-27 17:22:20 +0100

    Loop logic

--- client/index.ts
+++ client/index.ts
@@ -1,6 +1,7 @@
 import { open } from "node:fs/promises";
 import { createInterface } from "node:readline";
 
+// reads 1000 lines from the dictionary
 const readDict = async (startingPoint: number): Promise<void> => {
     const dictHandle = await open("./dict.txt", "r");
     const readable = dictHandle.createReadStream();
@@ -10,10 +11,10 @@ const readDict = async (startingPoint: number): Promise<void> => {
         const result: Array<string> = [];
         let counter = 0;
         reader.on('line', (line) => {
-            if (counter >= startingPoint && counter < startingPoint + 100) {
+            if (counter >= startingPoint && counter < startingPoint + 1000) {
                 result.push(line);
                 counter++;
-            } else if (counter < startingPoint + 100) {
+            } else if (counter < startingPoint + 1000) {
                 counter++;
             } else {
                 reader.close();
@@ -27,22 +28,45 @@ const readDict = async (startingPoint: number): Promise<void> => {
     // we ignore words longer than 3 chars
     lines = lines.filter((word) => word.length > 2);
 
-    console.log(lines.join())
     reader.close()
     readable.close();
     dictHandle.close();
 }
 
+// saves the current progress
 const save = async (count: number): Promise<void> => {
     const saveHandle = await open("./save.txt", "w");
     await saveHandle.writeFile(`${count}`)
     saveHandle.close()
 };
 
+// loads the current progress
 const load = async (): Promise<number> => {
     const saveHandle = await open("./save.txt", "r");
     const content = await saveHandle.readFile();
-    console.log(parseInt(content.toString()));
     saveHandle.close();
     return parseInt(content.toString());
 }
+
+// wrapper around setTimeout
+const timeout = async (ms: number): Promise<void> => {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+const main = async (): Promise<void> => {
+    let progress = 0;
+    try {
+        progress = await load();
+    } catch {
+        console.log("No save.txt found, starting at index 0");
+    }
+
+    while (progress < 370105) {
+        await readDict(progress);
+        progress = progress + 1000
+        await save(progress);
+        await timeout(60000)
+    }
+}
+
+main()
